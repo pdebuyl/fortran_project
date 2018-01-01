c396e35cf43d4692ab0322a612ec373a0259f5a0
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-01-01 22:49:59 +0100

    use datetime

--- src/fortran_program.f90
+++ src/fortran_program.f90
@@ -1,6 +1,11 @@
 program fortran_program
+  use datetime_module, only: datetime, timedelta, clock
   implicit none
 
+  type(datetime) :: a
+
   write(*,*) 'Hello, world!'
+  a = a%now()
+  write(*,*) 'The time is '//a%isoformat()
 
 end program fortran_program
